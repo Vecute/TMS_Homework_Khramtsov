abd5ae5060f6c8c72333948e3b14b15c03494a92
Gleb Khramtsov <vecutemoffy@gmail.com>
2024-05-28 21:16:46 +0300

    little improvements

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { PostCard, PostProps } from './PostCard';
 import '../styles/PostList.scss';
 
