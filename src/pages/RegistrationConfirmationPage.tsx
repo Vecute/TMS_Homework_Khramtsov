cb94d72a0486e3127a0d9611af7ecfedd9457f74
Gleb Khramtsov <vecutemoffy@gmail.com>
2024-06-18 20:04:36 +0300

    added a check for the accessibility of the link to the page in the URL

--- src/pages/RegistrationConfirmationPage.tsx
+++ src/pages/RegistrationConfirmationPage.tsx
@@ -1,12 +1,23 @@
 import TemplatePage from "./TemplatePage";
 import RegistrationConfirmation from "../components/RegistrationConfirmation";
+import { useNavigate, useNavigationType } from 'react-router-dom';
+import { useEffect } from "react";
 
 function RegistrationConfirmationPage() {
-    return (
-      <TemplatePage title="Registration Confirmation">
-        <RegistrationConfirmation />
-      </TemplatePage>
-    );
-  }
+  const navigationType = useNavigationType(); // Вызов хука useNavigationType для получения типа навигации
+  const navigate = useNavigate(); // Вызов хука useNavigate для получения функции перенаправления
 
-export default RegistrationConfirmationPage
+  useEffect(() => {
+    if (navigationType !== 'PUSH') { // Если пользователь попал на страницу не через navigate()
+      navigate('/'); // Перенаправляем пользователя на главную страницу
+    }
+  }, [navigationType, navigate]);
+
+  return (
+    <TemplatePage title="Registration Confirmation">
+      <RegistrationConfirmation />
+    </TemplatePage>
+  );
+}
+
+export default RegistrationConfirmationPage;
